397f4f2b4784de9a61918d29a00425ef677d1916
Mayank Jain <mayank.jain@daimler.com>
2020-11-04 18:30:45 +0530

    Change in include/ structure

--- my_project/my_math/src/addition.cpp
+++ my_project/my_math/src/addition.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "addition.h"
+#include "my_math/addition.h"
 
 float addition( float num1, float num2 ){
 	return num1+num2+0;
--- my_project/my_math/src/division.cpp
+++ my_project/my_math/src/division.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "division.h"
+#include "my_math/division.h"
 
 float division(float num1, float num2){
 	return num1/num2;
